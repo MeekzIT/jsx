1a454bf1b909440923174a217625b305327776c9
arssim001 <simonyanarseniy01@gmail.com>
2025-01-25 10:47:09 +0400

    update bank ante chips

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "46.202.189.245", // Set the host to your desired IP
+    port: 5173, // Specify the port
     open: true, // This will open the browser automatically
   },
 });
