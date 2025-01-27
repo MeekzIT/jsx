0c63d0a0431aa7ef114b064c2151df9bb43f8226
arssim001 <simonyanarseniy01@gmail.com>
2025-01-27 14:53:28 +0400

    update bank ante chips

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -17,32 +17,24 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
     speed: 500, // Transition speed
     slidesToShow: 1, // Number of slides visible at once
     slidesToScroll: 1, // Number of slides to scroll per click
-    autoplay: true, // Auto-scroll
-    autoplaySpeed: 3000, // Speed of auto-scroll
+    // autoplay: true, // Auto-scroll
+    // autoplaySpeed: 3000, // Speed of auto-scroll
   };
 
   return (
     <div>
       <Slider {...settings}>
         {images?.map((img, index) => (
-          <div
+          <img
             key={index}
             onClick={() => {
               setCurrent(img);
               setOpen(true);
             }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src={img.image}
-              alt={`Slide ${index + 1}`}
-              // style={{ width: "100%", maxHeight: 400 }}
-            />
-          </div>
+            src={img.image}
+            alt={`Slide ${index + 1}`}
+            // style={{ width: "100%", maxHeight: 400 }}
+          />
         ))}
       </Slider>
       <SimpleDialog open={open} onClose={() => setOpen(false)}>
